c0b0867f8a90c885c4ef0f7afba9a53b9ef154e3
martin <martin.sillence@jhc.co.uk>
2023-04-17 15:32:36 +0100

    sonar fixes

--- journal-parsing/src/main/java/com/fnz/db2/journal/test/JournalFilterTimeout.java
+++ journal-parsing/src/main/java/com/fnz/db2/journal/test/JournalFilterTimeout.java
@@ -25,50 +25,50 @@ import com.fnz.db2.journal.retrieve.rnrn0200.DetailedJournalReceiver;
 import com.ibm.as400.access.AS400;
 
 public class JournalFilterTimeout {
-	private static final Logger log = LoggerFactory.getLogger(CommitLogProcessor.class);
-	
+	private static final Logger log = LoggerFactory.getLogger(JournalFilterTimeout.class);
+
 	private static SchemaCacheHash schemaCache = new SchemaCacheHash();
 
 	public static void main(String[] args) throws Exception {
-	    TestConnector connector = new TestConnector();
-	    Connect<AS400, IOException> as400Connect = connector.getAs400();
-	    Connect<Connection, SQLException> sqlConnect = connector.getJdbc();
-	    String schema = connector.getSchema();
-	    
-        JournalInfo journalLib = JournalInfoRetrieval.getJournal(as400Connect.connection(), schema);
+		final TestConnector connector = new TestConnector();
+		final Connect<AS400, IOException> as400Connect = connector.getAs400();
+		final Connect<Connection, SQLException> sqlConnect = connector.getJdbc();
+		final String schema = connector.getSchema();
 
-        String offset =  System.getenv("ISERIES_OFFSET");
-        String receiver =  System.getenv("ISERIES_RECEIVER");
-        List<FileFilter> includes = new ArrayList<FileFilter>();
-        String includesEnv = System.getenv("ISERIES_INCLUDES");
-        if (includesEnv != null) {
-			for (String i : Arrays.asList(includesEnv.split(","))) {
+		final JournalInfo journalLib = JournalInfoRetrieval.getJournal(as400Connect.connection(), schema);
+
+		final String offset = System.getenv("ISERIES_OFFSET");
+		final String receiver = System.getenv("ISERIES_RECEIVER");
+		final List<FileFilter> includes = new ArrayList<>();
+		final String includesEnv = System.getenv("ISERIES_INCLUDES");
+		if (includesEnv != null) {
+			for (final String i : Arrays.asList(includesEnv.split(","))) {
 				includes.add(new FileFilter(schema, i));
 			}
 		}
 
-		JournalInfoRetrieval journalInfoRetrieval = new JournalInfoRetrieval();
-		List<DetailedJournalReceiver> receivers = journalInfoRetrieval.getReceivers(as400Connect.connection(), journalLib);
-		DetailedJournalReceiver first = receivers.stream().min((x, y) -> x.start().compareTo(y.start())).get();
-        JournalPosition endPosition = journalInfoRetrieval.getCurrentPosition(as400Connect.connection(), journalLib);
+		final JournalInfoRetrieval journalInfoRetrieval = new JournalInfoRetrieval();
+		final List<DetailedJournalReceiver> receivers = journalInfoRetrieval.getReceivers(as400Connect.connection(),
+				journalLib);
+		final DetailedJournalReceiver first = receivers.stream().min((x, y) -> x.start().compareTo(y.start())).get();
+		final JournalPosition endPosition = journalInfoRetrieval.getCurrentPosition(as400Connect.connection(),
+				journalLib);
 		log.info("start {} end {}", first, endPosition);
-		
-		
-		
-		try (PrintWriter pw = new PrintWriter(new File("exceptions.txt"))) {
-			JournalInfo journal = JournalInfoRetrieval.getJournal(as400Connect.connection(), schema);
-			log.info("journal: " + journal);
-			RetrieveConfig config = new RetrieveConfigBuilder().withAs400(as400Connect).withJournalInfo(journal).withDumpFolder("./bad-journal").withServerFiltering(true).withIncludeFiles(includes).build();
-			RetrieveJournal rj = new RetrieveJournal(config, journalInfoRetrieval);
 
-			JournalPosition p = new JournalPosition(first.start(), first.info().name(), first.info().library(), false); 
-			long start = System.currentTimeMillis();
-			boolean success = rj.retrieveJournal(p);
-			long end = System.currentTimeMillis();
-//			while (r.nextEntry()) {
-//				;
-//			}
-			log.info("success: {} position: {} time: {} ", success, rj.getPosition(), (end-start)/1000.0);
+		try (PrintWriter pw = new PrintWriter(new File("exceptions.txt"))) {
+			final JournalInfo journal = JournalInfoRetrieval.getJournal(as400Connect.connection(), schema);
+			log.info("journal: {}", journal);
+			final RetrieveConfig config = new RetrieveConfigBuilder().withAs400(as400Connect).withJournalInfo(journal)
+					.withDumpFolder("./bad-journal").withServerFiltering(true).withIncludeFiles(includes).build();
+			final RetrieveJournal rj = new RetrieveJournal(config, journalInfoRetrieval);
+
+			final JournalPosition p = new JournalPosition(first.start(), first.info().name(), first.info().library(),
+					false);
+			final long start = System.currentTimeMillis();
+			final boolean success = rj.retrieveJournal(p);
+			final long end = System.currentTimeMillis();
+
+			log.info("success: {} position: {} time: {} ", success, rj.getPosition(), (end - start) / 1000.0);
 
 		}
 	}
