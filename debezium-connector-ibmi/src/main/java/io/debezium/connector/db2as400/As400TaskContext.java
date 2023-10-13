c125586f641e7441f1c11124095f9cff0d8ac681
martin <martin.sillence@jhc.co.uk>
2023-10-13 10:36:06 +0100

    2.4 upgrade

--- debezium-connector-ibmi/src/main/java/io/debezium/connector/db2as400/As400TaskContext.java
+++ debezium-connector-ibmi/src/main/java/io/debezium/connector/db2as400/As400TaskContext.java
@@ -5,17 +5,20 @@
  */
 package io.debezium.connector.db2as400;
 
+import java.util.Map;
+
 import io.debezium.connector.common.CdcSourceTaskContext;
 
 public class As400TaskContext extends CdcSourceTaskContext {
-    private final As400ConnectorConfig config;
+	private final As400ConnectorConfig config;
 
-    public As400TaskContext(As400ConnectorConfig config, As400DatabaseSchema schema) {
-        super(config.getContextName(), config.getLogicalName(), schema::tableIds);
-        this.config = config;
-    }
+	public As400TaskContext(As400ConnectorConfig config, As400DatabaseSchema schema,
+			Map<String, String> customMetricTags) {
+		super(config.getContextName(), config.getLogicalName(), customMetricTags, schema::tableIds);
+		this.config = config;
+	}
 
-    public As400ConnectorConfig getConfig() {
-        return config;
-    }
+	public As400ConnectorConfig getConfig() {
+		return config;
+	}
 }
