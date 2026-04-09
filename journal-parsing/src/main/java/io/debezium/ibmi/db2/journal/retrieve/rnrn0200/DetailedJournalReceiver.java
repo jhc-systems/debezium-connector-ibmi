f54732f7cf6a870a101b00d094fb86e2ecec443d
martin <martin.sillence@jhc.co.uk>
2026-04-09 16:16:25 +0100

    fix: TT-1115 use delayed offset not current offset for metrics and use receiver list to calcuate range
    also needs the isSameReceiver

--- journal-parsing/src/main/java/io/debezium/ibmi/db2/journal/retrieve/rnrn0200/DetailedJournalReceiver.java
+++ journal-parsing/src/main/java/io/debezium/ibmi/db2/journal/retrieve/rnrn0200/DetailedJournalReceiver.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.debezium.ibmi.db2.journal.retrieve.JournalPosition;
 import io.debezium.ibmi.db2.journal.retrieve.JournalProcessedPosition;
 import io.debezium.ibmi.db2.journal.retrieve.JournalReceiver;
 
@@ -27,6 +28,13 @@ public record DetailedJournalReceiver(JournalReceiverInfo info, BigInteger start
                 this.maxEntryLength, this.numberOfEntries);
     }
 
+    public boolean isSameReceiver(JournalPosition position) {
+        if (info == null || position == null) {
+            return false;
+        }
+        return info.receiver().equals(position.getReceiver());
+    }
+    
     public boolean isSameReceiver(JournalProcessedPosition position) {
         if (info == null || position == null) {
             return false;
